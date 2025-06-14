ba01f013f114668fd72edfe15f6cc61ae327966c
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-06-14 14:12:26 -0700

    wip

--- download.R
+++ download.R
@@ -12,19 +12,10 @@ if (!file.exists(MIP_BR)) {
   system(glue("wget {STEM}/{MIP_BR}"))
 }
 
-raw <- read_xlsx(MIP_BR, col_names = FALSE)
-mip_br <- raw # Copy for edition
+mip_br <- read_xlsx(MIP_BR, col_names = FALSE)
 
-# Get headers
-h5_regions <- as.character(raw[5, ])
-h6_sectors <- as.character(raw[6, ])
-
-# Remove last two rows
-mip_br <- mip_br[-(nrow(raw) - 1):-nrow(raw), ]
-
-# Remove first six rows
-mip_br <- mip_br[-1:-6, ]
-
+# Remove the last 2 rows
+mip_br <- mip_br |> slice(-(n() - 1):-n())
 
 # Remove second to last row
 row_target <- unlist(mip_br[(nrow(mip_br) - 1), ])
@@ -36,10 +27,27 @@ col_target <- unlist(mip_br[, (ncol(mip_br) - 1)])
 stopifnot("Unexpected Data" = all(is.na(col_target)))
 mip_br <- mip_br[, -(ncol(mip_br) - 1)]
 
-# Row and Col keys
+# Get col headers
+h5_regions <- as.character(mip_br[5, ])
+h6_sectors <- as.character(mip_br[6, ])
+h5_regions[is.na(h5_regions)] <- ""
+h6_sectors[is.na(h6_sectors)] <- ""
+
+# Remove the first 6 rows
+mip_br <- mip_br |>
+  slice(-1:-6)
+
+# Ger row headers
+mip_br[[1]][is.na(mip_br[[1]])] <- ""
+mip_br[[2]][is.na(mip_br[[2]])] <- ""
+
 col_keys <- str_c(h5_regions, " ::: ", h6_sectors)
 row_keys <- str_c(mip_br[[1]], " ::: ", mip_br[[2]])
+
 
 # Drop first two cols
 mip_br <- mip_br |>
   select(-...1, -...2)
+
+mip_br <- mip_br |>
+  mutate(across(everything(), as.double))
