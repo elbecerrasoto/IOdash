6231fca07791b14ad8294021de275a2b195f35cd
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-06-14 14:59:43 -0700

    generating mip_br.tsv

--- download.R
+++ download.R
@@ -14,40 +14,49 @@ if (!file.exists(MIP_BR)) {
 
 mip_br <- read_xlsx(MIP_BR, col_names = FALSE)
 
-# Remove the last 2 rows
-mip_br <- mip_br |> slice(-(n() - 1):-n())
+# Remove the last 4 rows
+# Metadata and Total and Empty
+mip_br <- mip_br |> slice(-(n() - 3):-n())
 
-# Remove second to last row
-row_target <- unlist(mip_br[(nrow(mip_br) - 1), ])
-stopifnot("Unexpected Data" = all(is.na(row_target)))
-mip_br <- mip_br[-(nrow(mip_br) - 1), ]
+# Remove the last 2 columns
+mip_br <- mip_br[-(nrow(mip_br) - 1):-nrow(mip_br)]
 
-# Remove second to last col
-col_target <- unlist(mip_br[, (ncol(mip_br) - 1)])
-stopifnot("Unexpected Data" = all(is.na(col_target)))
-mip_br <- mip_br[, -(ncol(mip_br) - 1)]
+# Extract row and col names
+# row keys and col keys
+ck5 <- mip_br |>
+  slice(5) |>
+  as.character()
 
-# Get col headers
-h5_regions <- as.character(mip_br[5, ])
-h6_sectors <- as.character(mip_br[6, ])
-h5_regions[is.na(h5_regions)] <- ""
-h6_sectors[is.na(h6_sectors)] <- ""
+ck6 <- mip_br |>
+  slice(6) |>
+  as.character()
+
+ck5[is.na(ck5)] <- ""
+ck6[is.na(ck6)] <- ""
+
+rk1 <- mip_br[[1]]
+rk2 <- mip_br[[2]]
+
+rk1[is.na(rk1)] <- ""
+rk2[is.na(rk2)] <- ""
+
+row_keys <- str_c(rk1, " ::: ", rk2)
+col_keys <- str_c(ck5, " ::: ", ck6)
 
 # Remove the first 6 rows
-mip_br <- mip_br |>
-  slice(-1:-6)
-
-# Ger row headers
-mip_br[[1]][is.na(mip_br[[1]])] <- ""
-mip_br[[2]][is.na(mip_br[[2]])] <- ""
-
-col_keys <- str_c(h5_regions, " ::: ", h6_sectors)
-row_keys <- str_c(mip_br[[1]], " ::: ", mip_br[[2]])
-
+mip_br <- mip_br |> slice(-1:-6)
+row_keys <- row_keys[-1:-6]
 
 # Drop first two cols
-mip_br <- mip_br |>
-  select(-...1, -...2)
+mip_br <- mip_br[-1:-2]
+col_keys <- col_keys[-1:-2]
 
+# Make everything numeric
 mip_br <- mip_br |>
   mutate(across(everything(), as.double))
+
+
+names(mip_br) <- col_keys
+mip_br$row_key <- row_keys
+
+write_tsv(mip_br, "mip_br.tsv")
