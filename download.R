70678df4b314ebfcc70172e678bd9138325ef252
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-06-14 10:09:59 -0700

    remove NAs before totals

--- download.R
+++ download.R
@@ -9,36 +9,28 @@ STEM <- "https://www.inegi.org.mx/contenidos/investigacion/coumip/tabulados"
 MIP_BR <- "mip_ixi_br_sin_d_2018.xlsx"
 
 if (!file.exists(MIP_BR)) {
-  system(glue("wget {STEM}/{MIP_BR_SIN}"))
+  system(glue("wget {STEM}/{MIP_BR}"))
 }
 
 raw <- read_xlsx(MIP_BR, col_names = FALSE)
-h5_regions <- as.character(x[5, ])
-h6_sectors <- as.character(x[6, ])
+mip_br <- raw # Copy for edition
 
-mip_br <- raw
-tail(raw)
-str_c(h5_regions, " ::: ", h6_sectors)
+# Get headers
+h5_regions <- as.character(raw[5, ])
+h6_sectors <- as.character(raw[6, ])
 
-SKIP <- 6
-?read_xlsx
-mip_br_sin <- read_xlsx(MIP_BR_SIN, col_names = FALSE, skip = 6)
+# Remove first six rows
+mip_br <- mip_br[-1:-6, ]
 
-mip_br_sin |> tail(10)
+# Remove last two rows
+mip_br <- mip_br[-(nrow(raw) - 1):-nrow(raw), ]
 
+# Remove second to last row
+row_target <- unlist(mip_br[(nrow(mip_br) - 1), ])
+stopifnot("Unexpected Data" = all(is.na(row_target)))
+mip_br <- mip_br[-(nrow(mip_br) - 1), ]
 
-dim(x)
-
-HEADERS <-
-  mip_br_sin <- read_xlsx(MIP_BR_SIN, col_names = FALSE, skip = 6)
-
-# rm totals and NA rows
-nr <- nrow(mip_br_sin)
-mip_br_sin <- mip_br_sin[-(nr - 4):-nr, ]
-
-# h1 <- h1 |> str_replace("Resto del País", "rest") |> str_replace("Sinaloa", "sin")
-h2 <- str_replace(h2, "Resto del País", "rest")
-
-h2
-h2_sectors <-
-  view(mip_br_sin)
+# Remove second to last col
+col_target <- unlist(mip_br[, (ncol(mip_br) - 1)])
+stopifnot("Unexpected Data" = all(is.na(col_target)))
+mip_br <- mip_br[, -(ncol(mip_br) - 1)]
